7ef6cce2f049f0b142235eecee91cf7fdd5b1107
Tony Coan <tcoan98@gmail.com>
2013-04-13 17:14:45 -0700

    Added Permit and Miner classes

--- net/lotrcraft/minepermit/miner/Miner.java
+++ net/lotrcraft/minepermit/miner/Miner.java
@@ -1,5 +1,29 @@
 package net.lotrcraft.minepermit.miner;
 
-public class Miner {
+import java.util.ArrayList;
 
+import org.bukkit.Location;
+
+public class Miner{
+	
+	public Miner(String name){
+		
+		this.name = name;
+		permits = new ArrayList<Permit>();
+		
+	}
+	
+	private ArrayList<Permit> permits;
+	private String name;
+	
+	public Permit getPermit(Location l)
+	{
+		for(Permit p : permits){
+			if(p.contains(l))
+				return p;
+		}	
+	}
+	
+	
+	
 }
---
+++ net/lotrcraft/minepermit/miner/Permit.java
@@ -0,0 +1,12 @@
+package net.lotrcraft.minepermit.miner;
+
+import org.bukkit.Location;
+
+public class Permit {
+
+	public boolean contains(Location l) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
