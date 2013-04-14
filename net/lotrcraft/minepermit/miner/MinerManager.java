f180afa41298ede5bd78c85f14cfe445bd008566
Francesco Macagno <fjmacagno1@yahoo.com>
2013-04-13 17:14:34 -0700

    Added MinerManage

---
+++ net/lotrcraft/minepermit/miner/MinerManager.java
@@ -0,0 +1,33 @@
+package net.lotrcraft.minepermit.miner;
+
+import java.util.ArrayList;
+
+import net.lotrcraft.minepermit.MinePermit;
+
+import org.bukkit.Location;
+
+public class MinerManager {
+	
+	private ArrayList<Miner> miners;
+	private MinePermit minepermmit;
+	
+	public MinerManager(MinePermit m){
+		this.minepermmit = m;
+		
+		miners = new ArrayList<Miner>();
+	}
+	
+	public Miner getMiner(String name){
+		for(Miner m : miners){
+			if(m.getName().equals(name))
+				return m;
+		}
+	}
+	
+	public Permit getPermit(Location l){
+		for(Miner m : miners){
+			Permit p = m.getPermit(l);
+			if(p != null) return p;
+		}
+	}
+}
