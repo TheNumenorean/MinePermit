ab2ab1a236e87402199728d51e6f3140d7f43265
Francesco Macagno <fjmacagno1@yahoo.com>
2013-05-29 19:28:24 -0700

    Added

---
+++ src/net/lotrcraft/minepermit/world/PermitWorldManager.java
@@ -0,0 +1,28 @@
+package net.lotrcraft.minepermit.world;
+
+import java.util.TreeMap;
+
+public class PermitWorldManager {
+	
+	private TreeMap<String, PermitWorld> worlds;
+	
+	public PermitWorldManager(){
+		worlds = new TreeMap<String, PermitWorld>();
+	}
+	
+	public void addPermitWorld(PermitWorld pw){
+		worlds.put(pw.getWorld().getName(), pw);
+	}
+	
+	public PermitWorld getPermitWorld(String name){
+		return worlds.get(name);
+	}
+
+	/**
+	 * @return the worlds
+	 */
+	public TreeMap<String, PermitWorld> getWorlds() {
+		return worlds;
+	}
+
+}
